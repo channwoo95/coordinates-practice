795735da117aada6c25166ccefdb5ba9cfeec46b
chanwoo95 <60701519+chanwoo95@users.noreply.github.com>
2020-08-14 18:44:03 +0900

    update

--- main.js
+++ main.js
@@ -2,18 +2,22 @@ const horizontal = document.querySelector(".horizontal");
 const vertical = document.querySelector(".vertical");
 const target = document.querySelector(".target");
 const coordinates = document.querySelector(".coordinates");
+const targetRect = target.getBoundingClientRect();
+const targetHalfWidth = targetRect.width / 2;
+const targetHalfHeight = targetRect.height / 2;
 
-document.addEventListener("mousemove", (event) => {
-  const x = event.clientX;
-  const y = event.clientY;
+addEventListener("load", () => {
+  document.addEventListener("mousemove", (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
 
-  vertical.style.left = `${x}px`;
-  horizontal.style.top = `${y}px`;
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
 
-  target.style.left = `${x}px`;
-  target.style.top = `${y}px`;
+    target.style.transform = `translate(${x - targetHalfWidth}px, 
+      ${y - targetHalfHeight}px)`;
 
-  coordinates.style.left = `${x}px`;
-  coordinates.style.top = `${y}px`;
-  coordinates.innerHTML = `x: ${x}, y: ${y}`;
+    coordinates.style.transform = `translate(${x + 20}px, ${y + 20}px)`;
+    coordinates.innerHTML = `x: ${x}, y: ${y}`;
+  });
 });
